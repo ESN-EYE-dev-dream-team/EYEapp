e820615c14f85348b3ffd1bcff16f50b8e023c9a
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-02-10 16:42:43 +0100

    create event detail page in a form of ionic card

--- src/pages/eventPage/eventDetailPage/EventDetailPage.tsx
+++ src/pages/eventPage/eventDetailPage/EventDetailPage.tsx
@@ -1,10 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { IonBackButton, IonButtons, IonHeader, IonPage, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+import {
+    IonBackButton,
+    IonButtons,
+    IonHeader,
+    IonPage,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardContent,
+} from '@ionic/react';
 import { getEventsDetails } from '../EventPageAPI';
-import get from 'lodash/get';
 
 const EventDetailPage: React.FC = ({ match }: any) => {
-    const [eventData, setEventData] = useState({});
+    const [eventData, setEventData] = useState({
+        description: '',
+        cover: {
+            source: '',
+        },
+        start_time: '',
+        end_time: '',
+        name: '',
+        id: '',
+        place: {
+            name: '',
+        },
+    });
+
+    const { description, cover, start_time, end_time, name, place = { name: '-' } } = eventData;
+    const formattedDesciption = description.split('\n').map((item, key) => {
+        return (
+            <span key={key}>
+                {item}
+                <br />
+            </span>
+        );
+    });
 
     useEffect(() => {
         getEventsDetails(match.params.id).then(newEventData => {
@@ -19,13 +53,21 @@ const EventDetailPage: React.FC = ({ match }: any) => {
                     <IonButtons slot="start">
                         <IonBackButton defaultHref="/tab1" />
                     </IonButtons>
-                    <IonTitle>TEST</IonTitle>
+                    <IonTitle>Event detail</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <p>{get(eventData, 'name', '')}</p>
-                ----------------------------------
-                <p> eventDetails</p>
+                <IonCard>
+                    <IonCardHeader>
+                        <img src={cover.source} alt="" />
+                        <IonCardTitle> {name} </IonCardTitle>
+                        <IonCardSubtitle>Start: {start_time}</IonCardSubtitle>
+                        <IonCardSubtitle>End: {end_time}</IonCardSubtitle>
+                        <IonCardSubtitle>Place: {place.name}</IonCardSubtitle>
+                    </IonCardHeader>
+
+                    <IonCardContent>{formattedDesciption}</IonCardContent>
+                </IonCard>
             </IonContent>
         </IonPage>
     );
