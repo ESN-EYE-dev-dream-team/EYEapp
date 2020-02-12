846f64f2e5a3042625ffc4da482dbf050f204672
Daniel Kolas <daniel.kolas.author@gmail.com>
2020-02-12 21:24:31 +0100

    Embeded Practical Guide - to do: scalling according to the window size

--- src/pages/practicalGuideTab/PracticalGuideTab.tsx
+++ src/pages/practicalGuideTab/PracticalGuideTab.tsx
@@ -1,16 +1,28 @@
+
+
 import React from 'react';
+
+
+
 import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+
+
+const fullScreen = {
+      width: "100%",
+      height: "100%"
+    };
 
 const PracticalGuideTab: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonTitle>Practical Guide</IonTitle>
+                    <IonTitle class="ion-text-center">Practical Guide</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <IonList>PracticalGuideTab</IonList>
+                <iframe style={fullScreen} src="assets/pg/erasmus_pg.pdf"></iframe>
             </IonContent>
         </IonPage>
     );
