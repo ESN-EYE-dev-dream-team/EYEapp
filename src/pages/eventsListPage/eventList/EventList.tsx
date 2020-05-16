4c376fdf1675971d1ed70e2547f40c9441782b03
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-05-16 19:31:06 +0200

    Make linter happy

--- src/pages/eventsListPage/eventList/EventList.tsx
+++ src/pages/eventsListPage/eventList/EventList.tsx
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
 import { getAllEvents } from '../EventPageAPI';
-import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonList } from '@ionic/react';
-import { get } from 'lodash';
-import moment from 'moment';
+import { IonList } from '@ionic/react';
+// import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonList } from '@ionic/react';
+
+// import { get } from 'lodash';
+// import moment from 'moment';
 
 type EventListState = {
     eventList: Array<Record<string, any>>;
 };
 
-const createParsedDate = (dateString: any): string => {
-    if (!dateString) return '';
-    const parsedDate = moment(dateString);
-    return parsedDate.format('dddd, MMMM Do YYYY, h:mm a');
-};
+// const createParsedDate = (dateString: any): string => {
+//     if (!dateString) return '';
+//     const parsedDate = moment(dateString);
+//     return parsedDate.format('dddd, MMMM Do YYYY, h:mm a');
+// };
 
-const createEventElement = (eventData: any): JSX.Element => (
-    <IonItem routerLink={`/eventDetail/${eventData.id}`}>
-        <IonCard className="welcome-card">
-            <img src={eventData.cover.source} alt="" />
-            <IonCardHeader>
-                <IonCardTitle>{eventData.name}</IonCardTitle>
-                <IonCardSubtitle>
-                    Place: {get(eventData, 'place.name', '-')} @ {createParsedDate(eventData.start_time)}
-                </IonCardSubtitle>
-            </IonCardHeader>
-        </IonCard>
-    </IonItem>
-);
+// const createEventElement = (eventData: any): JSX.Element => (
+//     <IonItem routerLink={`/eventDetail/${eventData.id}`}>
+//         <IonCard className="welcome-card">
+//             <img src={eventData.cover.source} alt="" />
+//             <IonCardHeader>
+//                 <IonCardTitle>{eventData.name}</IonCardTitle>
+//                 <IonCardSubtitle>
+//                     Place: {get(eventData, 'place.name', '-')} @ {createParsedDate(eventData.start_time)}
+//                 </IonCardSubtitle>
+//             </IonCardHeader>
+//         </IonCard>
+//     </IonItem>
+// );
 
 class EventList extends Component<{}, EventListState> {
     state: EventListState = {
