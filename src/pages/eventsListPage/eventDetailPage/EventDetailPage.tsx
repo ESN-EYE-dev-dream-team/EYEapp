7d37c13803232b5c152daccbb5cacab1044ec27a
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-05-31 17:49:28 +0200

    Change eventDetailPage to modal

--- src/pages/eventsListPage/eventDetailPage/EventDetailPage.tsx
+++ src/pages/eventsListPage/eventDetailPage/EventDetailPage.tsx
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import {
-    IonBackButton,
-    IonButtons,
-    IonHeader,
-    IonPage,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonIcon,
-} from '@ionic/react';
-import { getEventsDetails } from '../EventPageAPI';
+import React from 'react';
+import { IonIcon } from '@ionic/react';
 import moment from 'moment';
-import './EventDetailPage.css';
-import { calendar, pin, text, time } from "ionicons/icons";
+import { calendar, pin, text, time } from 'ionicons/icons';
+
+import ModalHeader from 'utils/modalHeader/ModalHeader';
+import './EventDetailPage.scss';
 
 const createParsedDate = (dateString: any): string => {
     if (!dateString) return '';
@@ -20,23 +12,9 @@ const createParsedDate = (dateString: any): string => {
     return parsedDate.format('dddd, MMMM Do YYYY, h:mm a');
 };
 
-const EventDetailPage: React.FC = ({ match }: any) => {
-    const [eventData, setEventData] = useState({
-        description: '',
-        cover: {
-            source: '',
-        },
-        start_time: '',
-        end_time: '',
-        name: '',
-        id: '',
-        place: {
-            name: '',
-        },
-    });
-
-    const { description, cover, start_time, end_time, name, place = { name: '-' } } = eventData;
-    const formattedDesciption = description.split('\n').map((item, key) => {
+export default function EventDetailPage({ data, onDismiss }: { data: any; onDismiss: any }) {
+    const { description, cover, start_time, end_time, name, place = { name: '-' } } = data;
+    const formattedDesciption = description.split('\n').map((item: any, key: any) => {
         return (
             <span key={key}>
                 {item}
@@ -45,44 +23,30 @@ const EventDetailPage: React.FC = ({ match }: any) => {
         );
     });
 
-    useEffect(() => {
-        getEventsDetails(match.params.id).then(newEventData => {
-            setEventData(newEventData);
-        });
-    }, [match.params.id]);
-    //TODO: make structure to assign the data
     const startTimeArray = createParsedDate(start_time).split(',');
     const endTimeArray = createParsedDate(end_time).split(',');
 
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons slot="start">
-                        <IonBackButton defaultHref="/eventsList" />
-                    </IonButtons>
-                    <IonTitle>Event detail</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent>
-                <div className="event-details-container">
-                    <h2>{name}</h2>
-                    <img className="event-details-photo" src={cover.source} alt="" />
-                    <p className="ion-text-justify">
-                        <IonIcon icon={calendar} /> <strong>Date: </strong>{startTimeArray[0] + ', ' + startTimeArray[1]}
-                    </p>
-                    <p className="ion-text-justify">
-                        <IonIcon icon={time} /> <strong>Hour: </strong>{startTimeArray[startTimeArray.length - 1] + ' - ' + endTimeArray[endTimeArray.length - 1]}
-                    </p>
-                    <p className="ion-text-justify">
-                        <IonIcon icon={pin} /> <strong>Place: </strong>{place.name}
-                    </p>
-                    <hr className="event-details-divider" />
-                    <p className="ion-text-justify">{formattedDesciption}</p>
-                </div>
-            </IonContent>
-        </IonPage>
+        <div className="event-details-container">
+            <ModalHeader onClickHandler={onDismiss} />
+            <img className="event-details-photo" src={cover.source} alt="Event cover photo" />
+            <h2>{name}</h2>
+            <p className="ion-text-justify">
+                <IonIcon icon={calendar} /> <strong>Date: </strong>
+                {startTimeArray[0] + ', ' + startTimeArray[1]}
+            </p>
+            <p className="ion-text-justify">
+                <IonIcon icon={time} /> <strong>Hour: </strong>
+                {startTimeArray[startTimeArray.length - 1] + ' - ' + endTimeArray[endTimeArray.length - 1]}
+            </p>
+            <p className="ion-text-justify">
+                <IonIcon icon={pin} /> <strong>Place: </strong>
+                {place.name}
+            </p>
+            <p className="ion-text-justify">
+                <IonIcon icon={text} /> <strong>Description:</strong>
+                <br /> {formattedDesciption}
+            </p>
+        </div>
     );
-};
-
-export default EventDetailPage;
+}
