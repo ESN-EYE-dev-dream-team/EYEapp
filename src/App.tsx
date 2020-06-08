0dd0b9c01ad6b0598e3f2b45a8b256c8a9485b18
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-06-08 22:15:16 +0200

    Add assets to src. Change icon of selected tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
@@ -28,39 +28,62 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-    <IonApp>
-        <IonReactRouter>
-            <IonTabs>
-                <IonRouterOutlet>
-                    <Route path="/eventdetail/:id" component={EventDetailPage} exact={true} />
-                    <Route path="/eventslist" component={EventsListPage} exact={true} />
-                    <Route path="/officehours" component={OfficeHoursTab} exact={true} />
-                    <Route path="/esnmembers" component={ESNMembersTab} exact={true} />
-                    <Route path="/practicalGuide" component={PracticalGuideTab} exact={true} />
-                    <Route path="/" render={(): JSX.Element => <Redirect to="/eventslist" />} exact={true} />
-                </IonRouterOutlet>
-                <IonTabBar slot="bottom">
-                    <IonTabButton tab="eventsist" href="/eventsList">
-                        <IonIcon src="assets/icon/tabs/icon-calendar-full.svg" />
-                        <IonLabel>Events</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="officehours" href="/officehours">
-                        <IonIcon src="assets/icon/tabs/icon-clock-full.svg" />
-                        <IonLabel>Office Hours</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="esnmembers" href="/esnmembers">
-                        <IonIcon src="assets/icon/tabs/icon-person-full.svg" />
-                        <IonLabel>ESN Members</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="practicalGuide" href="/practicalGuide">
-                        <IonIcon src="assets/icon/tabs/icon-document-full.svg" />
-                        <IonLabel>Practical Guide</IonLabel>
-                    </IonTabButton>
-                </IonTabBar>
-            </IonTabs>
-        </IonReactRouter>
-    </IonApp>
-);
+import iconCalendarFull from './assets/tabs/icon-calendar-full.svg';
+import iconClockFull from './assets/tabs/icon-clock-full.svg';
+import iconPersonFull from './assets/tabs/icon-person-full.svg';
+import iconDocumentFull from './assets/tabs/icon-document-full.svg';
+
+import iconCalendarEmpty from './assets/tabs/icon-calendar-empty.svg';
+import iconClockEmpty from './assets/tabs/icon-clock-empty.svg';
+import iconPersonEmpty from './assets/tabs/icon-person-empty.svg';
+import iconDocumentEmpty from './assets/tabs/icon-document-empty.svg';
+
+const App: React.FC = () => {
+    const [selectedTab, setSelectedTab] = useState<String>('');
+
+    return (
+        <IonApp>
+            <IonReactRouter>
+                <IonTabs>
+                    <IonRouterOutlet>
+                        <Route path="/eventdetail/:id" component={EventDetailPage} exact={true} />
+                        <Route path="/eventsList" component={EventsListPage} exact={true} />
+                        <Route path="/officeHours" component={OfficeHoursTab} exact={true} />
+                        <Route path="/esnMembers" component={ESNMembersTab} exact={true} />
+                        <Route path="/practicalGuide" component={PracticalGuideTab} exact={true} />
+                        <Route path="/" render={(): JSX.Element => <Redirect to="/eventslist" />} exact={true} />
+                    </IonRouterOutlet>
+                    <IonTabBar slot="bottom">
+                        <IonTabButton tab="eventsList" href="/eventsList" onClick={() => setSelectedTab('eventsList')}>
+                            <IonIcon src={selectedTab === 'eventsList' ? iconCalendarEmpty : iconCalendarFull} />
+                            <IonLabel>Events</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton
+                            tab="officeHours"
+                            href="/officeHours"
+                            onClick={() => setSelectedTab('officeHours')}
+                        >
+                            <IonIcon src={selectedTab === 'officeHours' ? iconClockEmpty : iconClockFull} />
+                            <IonLabel>Office Hours</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton tab="esnMembers" href="/esnMembers" onClick={() => setSelectedTab('esnMembers')}>
+                            <IonIcon src={selectedTab === 'esnMembers' ? iconPersonEmpty : iconPersonFull} />
+                            <IonLabel>ESN Members</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton
+                            tab="practicalGuide"
+                            href="/practicalGuide"
+                            onClick={() => setSelectedTab('practicalGuide')}
+                        >
+                            <IonIcon src={selectedTab === 'practicalGuide' ? iconDocumentEmpty : iconDocumentFull} />
+
+                            <IonLabel>Practical Guide</IonLabel>
+                        </IonTabButton>
+                    </IonTabBar>
+                </IonTabs>
+            </IonReactRouter>
+        </IonApp>
+    );
+};
 
 export default App;
